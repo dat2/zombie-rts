715fed4ae527d8d9313c418f3b8c8b9990684b18
Nicholas Dujay <nickdujay@gmail.com>
2014-09-30 11:47:21 -0400

    added a simple movement code

--- zombie-rts/scripts/main.js
+++ zombie-rts/scripts/main.js
@@ -2,16 +2,54 @@ export default function start() {
   var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     preload: preload,
     create: create,
-    update: update
+    update: update,
+    render: render
   });
+  var sprite,
+    moveToPosition;
 
   function preload() {
     game.load.image('star', 'assets/images/star.png');
   }
 
   function create() {
-    game.add.sprite(50, 10, 'star');
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
+    sprite.anchor.set(0.5);
+
+    game.physics.arcade.enable(sprite);
+
+    sprite.inputEnabled = true;
+    sprite.events.onInputDown.add(listener, this);
+
+    moveToPosition = {
+      x: sprite.x,
+      y: sprite.y
+    };
   }
 
-  function update() {}
+  function listener() {
+    console.log(arguments);
+  }
+
+  function update() {
+    if(game.input.activePointer.isDown) {
+      console.log(game.input.activePointer);
+      moveToPosition = {
+        x: game.input.activePointer.x,
+        y: game.input.activePointer.y
+      };
+    }
+
+    if(game.physics.arcade.distanceToXY(sprite, moveToPosition.x, moveToPosition.y) > 6) {
+      game.physics.arcade.moveToObject(sprite, moveToPosition, 300);
+    } else {
+      sprite.body.velocity.set(0);
+    }
+  }
+
+  function render() {
+    game.debug.inputInfo(32, 32);
+  }
 }
