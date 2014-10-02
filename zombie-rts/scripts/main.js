383759bbf7f247de03a8cc39e71a28fa759d7dce
Nicholas Dujay <nickdujay@gmail.com>
2014-10-02 13:50:50 -0400

    added some comments for easier refactoring later

--- zombie-rts/scripts/main.js
+++ zombie-rts/scripts/main.js
@@ -44,7 +44,7 @@ export default function start() {
     sprite.inputEnabled = true;
   }
 
-  function setPosition(pixelPos) {
+  function updateUnitPosition(pixelPos) {
     var tilePos = state.pixelCoordsToTileCoords(pixelPos);
     unit.moveTo(tilePos);
 
@@ -52,27 +52,29 @@ export default function start() {
   }
 
   function update() {
-
-    //if they press the mouse, find a path to the nearest tile
     if(game.input.activePointer.isDown) {
+      //find the tile that the mouse clicked on
       var tilePos = state.pixelCoordsToTileCoords(game.input.activePointer);
+
+      //find a path to this tile (findPath adds to the pathQueue)
       unit.findPath(state.grid, tilePos);
     }
 
-    //pixel coords outside of unit
-    //should i move inside unit?
+    //while the sprite is not at the pixel position, keep moving
     var pixelPosition = state.tileCoordsToPixelCoords(unit.position);
-
     if(game.physics.arcade.distanceToXY(sprite, pixelPosition.x, pixelPosition.y) > 6) {
       game.physics.arcade.moveToObject(sprite, pixelPosition, 300);
       setPosOnce = false;
     } else {
 
+      //else stop moving, and update the units position to the new tile
       sprite.body.velocity.set(0);
       if(!setPosOnce) {
-        setPosition(sprite);
+        updateUnitPosition(sprite);
       }
 
+      // if the unit has more points in the pathQueue, set the next move position
+      // to the next point in the queue
       if(unit.pathQueue.length > 0) {
         if(unit.iterateOverPath()) {
           pixelPosition = state.tileCoordsToPixelCoords(unit.position);
