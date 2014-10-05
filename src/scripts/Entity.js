9c1dea8f95b941e508e1f5b140c778ce609f3435
Nicholas Dujay <nickdujay@gmail.com>
2014-10-05 18:43:42 -0400

    removed state from entity, added a speed attribute

--- src/scripts/Entity.js
+++ src/scripts/Entity.js
@@ -1,21 +1,16 @@
 export class Entity {
 
   // x, y are tile coordinates, not pixel coordinates
-  constructor({x, y, game, state}) {
+  constructor({ x, y, game , speed}) {
     this.position = {x, y};
     this.game = game;
-    this.state = state;
 
     // other attributes
     // constant acceleration
-    this.speed = 300;
+    this.speed = speed;
   }
 
-  moveTo({x, y}) {
-    this.position = {x, y};
-  }
-
-  updateWorldPosition() {
-
+  moveTo({ x, y }) {
+    this.position = { x, y };
   }
 }
