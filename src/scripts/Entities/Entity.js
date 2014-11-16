e530ead6cedca6c35117fff61ba51a3e154d2e78
Nicholas Dujay <nickdujay@gmail.com>
2014-11-15 21:18:47 -0500

    added health to entities

--- src/scripts/Entities/Entity.js
+++ src/scripts/Entities/Entity.js
@@ -2,24 +2,44 @@ var Phaser = window.Phaser;
 export default class Entity {
 
   // x, y are tile coordinates, not pixel coordinates
-  constructor({ x, y, game , speed, spriteKey}) {
+  constructor({ x, y, game, speed, spriteKey, maxHealth}) {
+    // physics variables
     this.position = { x, y };
     this.direction = { x: 0, y: 0 };
 
-    // other attributes
     // constant acceleration
     this.speed = speed;
+
+    game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+    this.sprite.body.collideWorldBounds = true;
 
     // visual stuff
     this.sprite = game.add.sprite(x, y, spriteKey);
     this.sprite.anchor.set(0.5);
 
-    game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
-    this.sprite.body.collideWorldBounds = true;
+    // not physics or visual related variables
+    this.maxHealth = maxHealth;
+    this.health = this.maxHealth;
+    this.dead = false;
   }
 
   setDirection({ x, y }) {
     this.direction = { x, y };
+  }
+
+  decreaseHealth(value) {
+    this.health -= value;
+    if(this.health < 0) {
+      this.health = 0;
+      this.dead = true;
+    }
+  }
+
+  increaseHealth(value) {
+    this.health += value;
+    if(this.health > this.maxHealth) {
+      this.health = this.maxHealth;
+    }
   }
 
   preUpdate() {
