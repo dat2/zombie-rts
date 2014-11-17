54c58c41b4c50c9dd8c40981c8d59a60033098f0
Nicholas Dujay <nickdujay@gmail.com>
2014-11-16 22:02:26 -0500

    added simple working zombie follow ai

--- src/scripts/AI/ZombieAI.js
+++ src/scripts/AI/ZombieAI.js
@@ -10,10 +10,10 @@ export class ZombieMovementAI extends MovementAI {
 
 // TODO figure out how to store map?
 export class ZombieAttackAI {
-  constructor({ zombie }) {
+  constructor({ zombie, game }) {
     this.zombie = zombie;
-
-    this.target = null;
+    this.game = game;
+    this.target = undefined;
   }
 
   update() {
@@ -24,14 +24,34 @@ export class ZombieAttackAI {
 
   // update target based on death, other zombies, etc.
   updateTarget() {
+    // if we have no target, find the closes
+    if(this.target === undefined ||
+      // if the target is dead, find another
+      (this.target !== undefined && this.target.health === 0)) {
 
+      let entities = this.game.entityManager.filterEntities( (entity) => entity instanceof Unit);
+      if(entities.length === 0) {
+        return;
+      }
+
+      let target = entities[0];
+      let dist = this.game.physics.arcade.distanceToXY;
+      let targetDist = dist(this.zombie.sprite, target.x, target.y);
+      entities.forEach( (entity) => {
+        let entityDist = dist(this.zombie.sprite, entity.sprite.x, entity.sprite.y);
+        if(entityDist < targetDist) {
+          targetDist = entityDist;
+          target = entity;
+        }
+      });
+      this.target = target;
+    }
   }
 
   moveTowardsTarget() {
     // move to the target
-    if(this.target) {
-      let { x, y } = target.sprite;
-      this.zombie.MoveAI.setTarget(target);
+    if(this.target !== undefined) {
+      this.zombie.MoveAI.findPathTo(this.game.map, this.target.position, false);
     }
   }
 
