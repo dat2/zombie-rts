bc11fa3c6af4eee5288a5fba28aa9f168cf7c508
Nicholas Dujay <nickdujay@gmail.com>
2014-11-21 18:36:06 -0500

    working on camera bugs

--- src/scripts/Camera/Camera.js
+++ src/scripts/Camera/Camera.js
@@ -4,7 +4,7 @@ export default class Camera extends Entity {
   constructor({x, y, spriteKey, speed}) {
     super({x, y, spriteKey, speed});
 
-    this.sprite.visible = false;
+    // this.sprite.visible = false;
     this.sprite.body.immovable = true; // no other entity should move this
   }
 }
